46c5248cbb4eec74f2f8657c655e02ae931d8fbe
forheroku <forherokuuse@gmail.com>
2018-08-09 23:58:21 -0400

    añadido servidor y puerto entregado o por default

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const aplicacion = express();
 
 const request = require('request');
 
+aplicacion.set('port', process.env.PORT || 3000);
+
+
 aplicacion.post('/api/:rut/:dve', (request, response) => {
     response.json({
         "rut": request.params.rut,
         "dve": request.params.dve
     });
+});
+
+aplicacion.listen(aplicacion.get('port'), () => {
+    console.log('Server connected on port ', aplicacion.get('port'));
 });
